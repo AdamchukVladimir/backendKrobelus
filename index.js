f1cf9c895c097435fdcc65d33e4ebf6047df9f1a
AdamchukVladimir <vldmr.adamchuk@gmail.com>
2023-04-09 22:49:07 +0300

    add STEAM authorization

--- index.js
+++ index.js
@@ -4,14 +4,55 @@ import multer from 'multer';
 import router from "./router.js";
 import fileUpload from 'express-fileupload';
 
+//STEAM
+import passport from 'passport';
+import session from 'express-session';
+import passportSteam from 'passport-steam';
 
 const PORT = 5000;
+const PORT_8080 = 8080;
 const DB_URL = `mongodb+srv://1virusafw1:Storm228322S@cluster1.wxnlurr.mongodb.net/?retryWrites=true&w=majority`;
 const app = express();
 
+//STEAM
+var SteamStrategy = passportSteam.Strategy;
+
+// Required to get data from user for sessions
+passport.serializeUser((user, done) => {
+    done(null, user);
+   });
+   passport.deserializeUser((user, done) => {
+    done(null, user);
+   });
+   // Initiate Strategy
+   passport.use(new SteamStrategy({
+    returnURL: 'http://localhost:' + PORT + '/api/auth/steam/return',
+    realm: 'http://localhost:' + PORT + '/',
+    apiKey: 'D1893E0A93FA327C5D749D6B9303C05E'
+    }, function (identifier, profile, done) {
+     process.nextTick(function () {
+      profile.identifier = identifier;
+      return done(null, profile);
+     });
+    }
+   ));
+   app.use(session({
+    secret: 'Whatever_You_Want',
+    saveUninitialized: true,
+    resave: false,
+    cookie: {
+     maxAge: 3600000
+    }
+   }))
+   app.use(passport.initialize());
+   app.use(passport.session());
+
+
+
 // CORS middleware
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080"); 
+    //res.header("Access-Control-Allow-Origin", "http://localhost:8080"); 
+    res.header("Access-Control-Allow-Origin", "*"); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
@@ -21,6 +62,21 @@ app.use(fileUpload({}));
 app.use('/api', router);
 // app.use(cors(corsOptions));//Должно вклчить корс 07.04.2023
 
+
+// Routes STEAM
+app.get('/', (req, res) => {
+    console.log("req.user " + JSON.stringify(req.user));
+    res.send(req.user);
+});
+app.get('/api/auth/steam', passport.authenticate('steam', {failureRedirect: '/'}), function (req, res) {
+    //res.redirect('/')
+    res.redirect('http://localhost:8080');
+});
+app.get('/api/auth/steam/return', passport.authenticate('steam', {failureRedirect: '/'}), function (req, res) {
+    //res.redirect('/')
+    console.log("req.user " + JSON.stringify(req.user));
+    res.redirect('http://localhost:8080');
+});
 
 
 
